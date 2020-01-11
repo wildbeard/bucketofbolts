ef771530152ddcca6a6523a5f40ad4f57a270821
Preston Haddock <quotablesarcasm@gmail.com>
2020-01-10 19:59:47 -0700

    App now correctly launches on windows

--- app/start.sh
+++ app/start.sh
@@ -5,4 +5,4 @@ cd /var/www/bucketofbolts
 pip install -r requirements.txt
 
 # can only access if --host=0.0.0.0 is set
-env FLASK_APP=bucketofbolts-api.py flask run --host=0.0.0.0
+env FLASK_APP=bucketofbolts-api.py FLASK_DEBUG=1 flask run --host=0.0.0.0
