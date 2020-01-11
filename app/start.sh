993c5de6deeb237b9a5662e633e3fda5e03c33a4
Preston Haddock <quotablesarcasm@gmail.com>
2020-01-10 19:15:00 -0700

    Updated Dockerfile to remove app dir

--- app/start.sh
+++ app/start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-cd /var/www/app
+cd /var/www/bucketofbolts
+
+pip install -r requirements.txt
 
 # can only access if --host=0.0.0.0 is set
 env FLASK_APP=bucketofbolts-api.py flask run --host=0.0.0.0
